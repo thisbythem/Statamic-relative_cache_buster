6aaad823793c6e4cd58bbf5bff5bc9c8cc41e7d8
James Wagner <james@thisbythem.com>
2015-01-09 09:41:49 -0800

    Bump version

--- relative_cache_buster/api.relative_cache_buster.php
+++ relative_cache_buster/api.relative_cache_buster.php
@@ -4,7 +4,7 @@ class API_relative_cache_buster extends API {
 
   public $meta = array(
     'name'       => 'Relative Cache Buster',
-    'version'    => '1.0.0',
+    'version'    => '1.1.0',
     'author'     => 'Jamie Wagner',
     'author_url' => 'http://thisbythem.com'
   );
